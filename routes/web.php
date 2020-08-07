87206cefb3b588f829ad380c6f547db28d543bdf
trymeforfun <okky.tirta@raharja.info>
2020-08-07 16:13:40 +0700

    add auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
-use App\Model\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Auth::routes();
 // Admin
 Route::get('/admin_login', 'Admin\AdminLoginController@showLoginForm');
 Route::post('/login', 'Admin\AdminLoginController@login');
@@ -27,6 +18,7 @@ Route::get('/', function () { return 'login'; });
 // user
 Route::get('_login', 'User\LoginController@showLoginForm');
 Route::post('_loginPost', 'User\LoginController@login');
+
 Route::get('user', 'User\UserController@index');
 Route::get('user/json', 'User\UserController@json')->name('user/json');
 Route::get('teacher', 'User\TeacherController@index');
@@ -34,7 +26,6 @@ Route::get('users', 'User\UserController@userShow');
 Route::post('user/store', 'User\UserController@store')->name('user/store');
 Route::get('user/edit/{id}', 'User\UserController@edit')->name('user/edit/{id}');
 Route::delete('user/delete/{id}', 'User\UserController@destroy')->name('user/delete/{id}');
-// user
 
 Route::resource('student', 'StudentController');
 Route::resource('lesson', 'LessonController');
@@ -48,7 +39,9 @@ Route::post('/changestatus', 'AssignmentController@changeStatus')->name('changes
 Route::get('/assignment/create', 'AssignmentController@create');
 Route::post('/assignment/store', 'AssignmentController@store');
 
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+// user
+
+
+// CLIENT
+
